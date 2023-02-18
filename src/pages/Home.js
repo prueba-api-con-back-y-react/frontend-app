de8a5080af38a0a52337c5bd53b094e28cc4b9d3
rubsuadav <rsuarezdavid@gmail.com>
2023-02-18 01:31:41 +0100

    crud and export data

---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactHTMLTableToExcel from "html-to-excel-react";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    setUsers(result.data);
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <ReactHTMLTableToExcel
+          id="test-table-xls-button"
+          className="download-table-xls-button"
+          table="table-to-xls"
+          filename="tablexls"
+          sheet="tablexls"
+          buttonText="Download as XLS"
+        />
+        <hr></hr>
+        <table className="table border shadow" id="table-to-xls">
+          <thead>
+            <tr>
+              <th scope="col">S.N</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  <a href={`/viewuser/${user.id}`}>{index + 1}</a>
+                </th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
